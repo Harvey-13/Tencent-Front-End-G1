3809697bc473b7223eb07bbe915928569065433a
Garfield-0927 <929780650@qq.com>
2021-11-24 22:59:22 +0800

    feat: delete normalize.css
    because antd.css includes normalize.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 // src/app.tsx
 import React from 'react';
 import './App.less';
-import 'normalize.css';
 import 'antd/dist/antd.css';
 import Comment from '@comp/Comment/Comment';
 const App: React.FC<any> = () => {
