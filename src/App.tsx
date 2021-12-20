bf8e0eeb254a7d0d1de2345b747e4fa57f801dd8
Garfield-0927 <929780650@qq.com>
2021-12-20 14:55:00 +0800

    chore: chore change

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,8 @@
 import React from 'react';
 import './App.less';
 import 'antd/dist/antd.css';
-import Comment from '@comp/Comment/Comment';
 const App: React.FC<any> = () => {
-  return (
-    <div className='app'>
-      <Comment />
-    </div>
-  );
+  return <div className='app'></div>;
 };
 
 export default App;
