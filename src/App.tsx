ca1553fad2b0ba3b7335d28a14baf80b003b7b18
muqiu1 <14600900@qq.com>
2021-12-24 16:32:11 +0800

    feat(api and utils): 增加网络请求api和utils

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.less';
 import './assets/css/common.less';
 import 'antd/dist/antd.css';
-import Home from '@view/Home/Home';
+import Home from '@/view/Home/home';
 
 const App: React.FC<any> = () => {
   return (
